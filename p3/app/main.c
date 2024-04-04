82a9caeea2af02cbee362fb4bd8411f0184ee299
yanxi6 <yanxi48@gmail.com>
2024-04-04 23:42:48 +0800

    p3 ready for test

--- p3/app/main.c
+++ p3/app/main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include "../src/lab.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
-  printf("hello world\n");
+  int c;
+  while ((c = getopt (argc, argv, "v")) != -1) {
+    switch (c)
+      {
+        case 'v':
+          fprintf (stderr, "lab_VERSION_MAJOR\n", lab_VERSION_MAJOR);
+          fprintf (stderr, "lab_VERSION_MINOR\n", lab_VERSION_MINOR);
+          break;
+        default:
+          abort ();
+      }
+  }
+    
   return 0;
 }
