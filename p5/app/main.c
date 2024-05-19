81fdb44ee0da56b29afa040426437de2841bc23e
yanxi6 <yanxi48@gmail.com>
2024-05-19 11:33:57 +0800

    Added in starter code

---
+++ p5/app/main.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../src/lab.h"
+#include <stdlib.h>
+#include <sys/time.h> /* for gettimeofday system call */
+#include "lab.h"
+
+
+
+int main(int argc, char **argv)
+{
+
+  if (argc < 3)
+    {
+      printf("usage: %s <array_size> <num_threads>", argv[0]);
+      return 1;
+    }
+  int size = atoi(argv[1]);
+  int t = atoi(argv[2]);
+
+  int *A_ = malloc(sizeof(int) *size);
+  srandom(1);
+  for (int i = 0; i < size; i++)
+    A_[i] = random() % 100000;
+
+  double end = 0;
+  double start = getMilliSeconds();
+  mergesort_mt(A_, size, t);
+  end = getMilliSeconds();
+  printf("%f %d\n",end-start, t);
+
+  return 0;
+}
+
+/**
+ * @brief Standard insertion sort that is faster than merge sort for small array's
+ *
+ * @param A The array to sort
+ * @param p The starting index
+ * @param r The ending index
+ */
+static void insertion_sort(int A[], int p, int r)
+{
+  int j;
+
+  for (j = p + 1; j <= r; j++)
+    {
+      int key = A[j];
+      int i = j - 1;
+      while ((i > p - 1) && (A[i] > key))
+        {
+	  A[i + 1] = A[i];
+	  i--;
+        }
+      A[i + 1] = key;
+    }
+}
+
+
+void mergesort_s(int A[], int p, int r)
+{
+  if (r - p + 1 <=  INSERTION_SORT_THRESHOLD)
+    {
+      insertion_sort(A, p, r);
+    }
+  else
+    {
+      int q = (p + r) / 2;
+      mergesort_s(A, p, q);
+      mergesort_s(A, q + 1, r);
+      merge_s(A, p, q, r);
+    }
+
+}
+
+void merge_s(int A[], int p, int q, int r)
+{
+  int *B = (int *)malloc(sizeof(int) * (r - p + 1));
+
+  int i = p;
+  int j = q + 1;
+  int k = 0;
+  int l;
+
+  /* as long as both lists have unexamined elements */
+  /*  this loop keeps executing. */
+  while ((i <= q) && (j <= r))
+    {
+      if (A[i] < A[j])
+        {
+	  B[k] = A[i];
+	  i++;
+        }
+      else
+        {
+	  B[k] = A[j];
+	  j++;
+        }
+      k++;
+    }
+
+  /* now only at most one list has unprocessed elements. */
+  if (i <= q)
+    {
+      /* copy remaining elements from the first list */
+      for (l = i; l <= q; l++)
+        {
+	  B[k] = A[l];
+	  k++;
+        }
+    }
+  else
+    {
+      /* copy remaining elements from the second list */
+      for (l = j; l <= r; l++)
+        {
+	  B[k] = A[l];
+	  k++;
+        }
+    }
+
+  /* copy merged output from array B back to array A */
+  k = 0;
+  for (l = p; l <= r; l++)
+    {
+      A[l] = B[k];
+      k++;
+    }
+
+  free(B);
+}
+
+double getMilliSeconds()
+{
+  struct timeval now;
+  gettimeofday(&now, (struct timezone *)0);
+  return (double)now.tv_sec * 1000.0 + now.tv_usec / 1000.0;
+}
